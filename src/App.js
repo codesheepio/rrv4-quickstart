26bc491e5591ee4817dbbf9bf8354a5aa53ec3d9
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-07-09 22:27:01 +0700

    Handle route mismatch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import './styles.css'
+const TopicList = props => <h1>TopicList</h1>
+const TopicDetail = ({ match }) =>
+  <h1>
+    Topic id: {match.params.id}
+  </h1>
+const NotFound = () => <h1>404 - Page not found</h1>
+const App = () =>
+  <div>
+    <h1>React router quickstart</h1>
+    <Link to="/topics">Topic List</Link>
+    <Link to="/topics/1">Topic Detail</Link>
+    <Link to="/notfound">Not found</Link>
+    <Switch>
+      <Redirect exact from="/" to="/topics" />
+      <Route path="/topics/:id" component={TopicDetail} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
 
-const App = () => <h1>Welcome to react-minimum-starter</h1>
 export default App
